92f15d004d6896bfe78722cb31118b53af01d604
Flisd <AgentBruceWane@gmail.com>
2025-12-13 14:10:19 -0600

    send some basic setup code to ensure that the Arduio can read the code from java via the COM signal

--- arduino/arduino/src/main.cpp
+++ arduino/arduino/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = 10;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+    delay(2000); // give Arduino time to reset (java closes the port too fast)
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (Serial.available() > 0) {
+        String input = Serial.readStringUntil('\n'); // read one line
+        for (int i = 0; i < input.length(); i++) {
+            char c = input[i];
+            if (c == '1') {
+                digitalWrite(ledPin, HIGH);
+            } else {
+                digitalWrite(ledPin, LOW);
+            }
+            delay(500); // 500ms per bit
+        }
+    } 
 }
